ab7f31e13c7160db03c49e9bcf6ba1ac6a0cf531
Stephanie Mark <stephaniemark@gmail.com>
2015-03-11 16:18:10 -0400

    first working copy of bash script to automate samtools sort

---
+++ bashtest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# iterate over files in a folder
+
+folder=$1
+
+for file in $folder*.bam
+do
+	echo $file
+	tag=$(echo $file | tr "." " " | awk '{print $1}' - )
+	sortname=$(echo $tag.sorted)
+	echo $sortname
+	samtools sort -n $file $sortname 2>$tag.sorting.err
+done
