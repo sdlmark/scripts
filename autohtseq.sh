47de00b3d2d7055a67f4eee9b6f81ed822a38ae4
Stephanie Mark <stephaniemark@gmail.com>
2015-03-12 14:56:03 -0400

    ready to run

--- autohtseq.sh
+++ autohtseq.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # run HTSeq on all files in a folder
 
+set -e
+set -o pipefail
+
 folder=$1
 
 for file in $folder*.sorted.bam
 do 
-	#echo $file
+	echo $file
 	tag=$(echo $file | tr "." " " | awk '{print $1}' )
-#	echo $tag.count
-#	command=$( htseq-count --format=bam --stranded=reverse --mode=intersection-nonempty --idattr=Parent --type=exon $file ~/c_briggsae.WBonly_exon.gff3 > $tag.count 2>$tag.err)
-#	echo $command
-	echo htseq-count --format=bam --stranded=reverse --mode=intersection-nonempty --idattr=Parent --type=exon $file ~/c_briggsae.WBonly_exon.gff3 \> $tag.count 2\> $tag.err
+	htseq-count --format=bam --stranded=reverse --mode=intersection-nonempty --idattr=Parent --type=exon $file ~/c_briggsae.WBonly_exon.gff3 > $tag.count 2> $tag.err
 done
