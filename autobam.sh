4e2c11c69af8bad5f3741c40b31c33e919719f23
Stephanie Mark <stephaniemark@gmail.com>
2015-03-13 12:31:56 -0400

    running this version of autobam.sh

--- autobam.sh
+++ autobam.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # shell script to automate conversion of SAM files into BAM files using samtools
 
+set -e
+set -o pipefail
+
 for file in $1*.sam
 do 
 	echo $file
-	id_tag=$(echo $file | tr "." " " )
+	id_tag=$(echo $file | tr "." " " | awk '{print $1}' )
 	echo $id_tag
+	command=$(samtools view -bS $file > $id_tag.Aligned.bam 2> $id_tag.Aligned.bam.err)
+	echo $command 
 done 
